972c96c1c7ad843dcf11a3f143b60fbbb5ef69f9
Karen Freeman-Smith <karenfreemansmith@gmail.com>
2017-02-05 17:55:43 -0800

    add more filter controls to apply multiple filters together

--- scripts.js
+++ scripts.js
@@ -7,7 +7,6 @@ const snap = document.querySelector('.snap');
 function getVideo() {
   navigator.mediaDevices.getUserMedia({ video: true, audio: false})
     .then(localMediaStream => {
-      console.log(localMediaStream);
       video.src = window.URL.createObjectURL(localMediaStream);
       video.play();
     })
@@ -25,11 +24,13 @@ function paintToCanvas() {
   return setInterval(() => {
     ctx.drawImage(video, 0, 0, width, height);
     let pixels = ctx.getImageData(0,0,width,height);
-    //pixels = redEffects(pixels);
-    //pixels = rgbSplit(pixels);
-    pixels = greenScreen(pixels);
+    pixels = colorEffects(pixels);
+    pixels = rgbSplit(pixels);
+    if(document.querySelector('h3 input').checked==true) {
+      pixels = greenScreen(pixels);
+    }
     ctx.putImageData(pixels, 0,0);
-  }, 25);
+  }, 16);
 }
 
 function takePhoto() {
@@ -44,20 +45,30 @@ function takePhoto() {
   strip.insertBefore(link, strip.firstChild);
 }
 
-function redEffects(pixels) {
+function colorEffects(pixels) {
+  const filters = {};
+  document.querySelectorAll('.filters input').forEach((input) => {
+    filters[input.name] = input.value;
+  });
+
   for(let i=0; i<pixels.data.length; i+=4) {
-    pixels.data[i+0] = pixels.data[i+0] +100; //RED
-    pixels.data[i+1] = pixels.data[i+1] -50;  //GREEN
-    pixels.data[i+2] = pixels.data[i+2] *0.5; //BLUE
+    pixels.data[i+0] = pixels.data[i+0] + parseInt(filters.redfilter); //RED
+    pixels.data[i+1] = pixels.data[i+1] + parseInt(filters.greenfilter);  //GREEN
+    pixels.data[i+2] = pixels.data[i+2] + parseInt(filters.bluefilter); //BLUE
   }
   return pixels;
 }
 
 function rgbSplit(pixels) {
+  const offsets = {};
+  document.querySelectorAll('.offset input').forEach((input) => {
+    offsets[input.name] = input.value;
+  });
+
   for(let i=0; i<pixels.data.length; i+=4) {
-    pixels.data[i-150] = pixels.data[i+0]; //RED
-    pixels.data[i+200] = pixels.data[i+1]; //GREEN
-    pixels.data[i-250] = pixels.data[i+2]; //BLUE
+    pixels.data[i-parseInt(offsets.split)/2] = pixels.data[i+0]; //RED
+    pixels.data[i+parseInt(offsets.split)] = pixels.data[i+1]; //GREEN
+    pixels.data[i-parseInt(offsets.split)] = pixels.data[i+2]; //BLUE
   }
   return pixels;
 }
@@ -68,6 +79,7 @@ function greenScreen(pixels) {
   document.querySelectorAll('.rgb input').forEach((input) => {
     levels[input.name] = input.value;
   });
+
   for(let i=0; i<pixels.data.length; i+=4) {
     red = pixels.data[i+0];
     green = pixels.data[i+1];
