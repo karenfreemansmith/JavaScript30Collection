b6a75bb36dc86c081502fdf4537bf7d8d0d8062a
Karen Freeman-Smith <karenfreemansmith@gmail.com>
2017-03-01 14:05:41 -0800

    debugged click event/clearInterval

--- scripts.js
+++ scripts.js
@@ -4,14 +4,12 @@ const endTime = document.querySelector('.display_end-time');
 const buttons = document.querySelectorAll('[data-time]');
 
 function timer(seconds) {
-  console.log(seconds);
   const now = Date.now();
   const then = now + seconds * 1000;
   displayTimeLeft(seconds);
   displayEndTime(then);
 
   countdown = setInterval(() => {
-    clearInterval(countdown);
     const secondsLeft = Math.round((then - Date.now())/1000);
     if(secondsLeft < 0) {
       clearInterval(countdown);
@@ -39,17 +37,20 @@ function displayEndTime(timestamp) {
   } else {
     endTime.textContent = `Be back at: ${hour}:${minutes < 10? '0':''}${minutes} (am)`;
   }
+}
 
 function startTimer() {
+  clearInterval(countdown);
   const seconds = parseInt(this.dataset.time);
   timer(seconds);
 }
 
+buttons.forEach(button => console.log(button));
 buttons.forEach(button => button.addEventListener('click', startTimer));
 document.customForm.addEventListener('submit', function(e) {
   e.preventDefault();
-  const mins = this.minutes.value;
+  const mins = parseInt(this.minutes.value);
+  clearInterval(countdown);
   timer(mins*60);
   this.reset();
 });
-}
